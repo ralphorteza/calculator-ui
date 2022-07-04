bc11e231f6c59f84cf65afcbac951016926cba6a
ralph orteza <ralphlorteza@gmail.com>
2022-07-04 15:26:05 -0700

    Remove data-key elements

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 window.addEventListener('keydown', function(e) {
   //const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  const pressedKey = e.key;
+  const key = this.document.querySelector('.key ' + pressedKey);
+  
   //if (!audio) { return; }
   //audio.currentTime = 0; // Rewind on start.
   // audio.play();
-  
-  key.classList.add('playing');
+  //console.log(pressedKey);
+  //btn.classList.add('playing');
 });
 
 function removeTransition(e) {
