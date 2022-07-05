2610e8a28ec60e70d80076a1adbe56b87886bc13
ralph orteza <ralphlorteza@gmail.com>
2022-07-04 17:01:43 -0700

    Modify operate() function

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-function calculate(input) {
-  let f = {
-            add: '+',
-            sub: '-',
-            div: '/',
-            mlt: '*',
-            mod: '%'
-          };
-
-  // only accept numbers and f values
-  input = input.replace(/[^0-9%^*\/()\-+.]/g, '');
-
-  console.log(input);
-}
-
-
-const add = (a,b) => { a + b; };
-const subtract = (a,b) => { a - b; };
-const multiply = (a,b) => { a * b; };
-const divide = (a,b) => { a / b; };
-const modulus = (a,b) => { a % b; };
 
 function operate(num1, num2, op) { 
+  result = num1 * 1;
+  if (op === '+') { result = num1 + num2; }
+  if (op === '-') { result = num1 - num2; }
+  if (op === '*') { result = num1 * num2; }
+  if (op === '%') { result = num1 % num2; }
+  if (op === '/') {
+    if (num2 === 0) { result = "undefined"; }
+    else { result = num1 / num2; }
+  }
 
+  return result;
 }
