49187f444e9550ae40d8ed7024a05899fb21c6f8
ralph orteza <ralphlorteza@gmail.com>
2022-07-16 00:28:48 -0700

    Modify script.js

--- script.js
+++ script.js
@@ -12,25 +12,3 @@ function operate(num1, num2, op) {
 
   return result;
 }
-/* window.addEventListener('keydown', function(e) {
-  //const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const pressedKey = e.key;
-  const key = this.document.querySelector('.key ' + pressedKey);
-  
-  //if (!audio) { return; }
-  //audio.currentTime = 0; // Rewind on start.
-  // audio.play();
-  //console.log(pressedKey);
-  //btn.classList.add('playing');
-});
- */
-
-
-function removeTransition(e) {
-  if (e.propertyName !== 'transform') return;
-  e.target.classList.remove('playing');
-}
-
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
