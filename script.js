d5f92f2050ce38d2c10f959951a2eea3cb7f3b3c
ralph orteza <ralphlorteza@gmail.com>
2022-07-17 00:12:17 -0700

    Refactor operate() and operator functions

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 
-function operate(num1, num2, op) { 
-  result = num1 * 1;
-  op = String(op);
-  if (op === '+') { result = num1 + num2; }
-  if (op === '-') { result = num1 - num2; }
-  if (op === '*') { result = num1 * num2; }
-  if (op === '%') { result = num1 % num2; }
-  if (op === '/') {
-    if (num2 === 0) { result = "undefined"; }
-    else { result = num1 / num2; }
-  }
+function operate(num1, num2, operator) { 
+  let op1 = parseFloat(num1);
+  let op2 = parseFloat(num2);
 
-  return result;
+  if (operator === "+") { return add(op1,op2); }
+  if (operator === "-") { return sub(op1,op2); }
+  if (operator === "*") { return mlt(op1,op2); }
+  if (operator === "/") { return div(op1,op2); }
+  if (operator === "%") { return mod(op1,op2); }
 }
+
+
+/* operator functions */
+const add = (a, b) => a + b;
+const sub = (a,b) => a - b;
+const mlt = (a,b) => a * b;
+const div = function(a,b) {
+  if (b !== 0) { return a / b;}
+  return "lol";
+}
+const mod = (a,b) => a % b;
 
 let str = "";
 let num1 = "";
 let num2 = "";
 let operator = "";
 let ans = "";
-
 console.log(num1);
 const btns = document.querySelectorAll('button');
 const display = document.querySelector('.display');
@@ -29,18 +35,10 @@ btns.forEach((button) => {
   button.addEventListener('click', (e) => {
     console.log('clicked');
     let value = button.textContent;
-
-    if (value === '=') {
-      ans = operate(num1, num2, operator);
-      updateDisplay(ans);
-    } else if (value === '+' || value === '-' || value === '*' 
-    || value === '/' || value === '%') {
-      operator = value;
-      updateDisplay(value);
-    } else if ()
-    num1 += value;
-    updateDisplay();
-    console.log(num1);
+    console.log(value);
+    str += value;
+    updateDisplay(str);
+    console.log(str);
 
   });
 });
