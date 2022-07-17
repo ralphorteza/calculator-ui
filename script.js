9d5d8fa4a05a61720568374b094bd68ac29348c3
ralph orteza <ralphlorteza@gmail.com>
2022-07-17 02:35:24 -0700

    Refactor event listener functions

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
+let str = "";
+let num1 = "";
+let num2 = "";
+let operator = "";
+let ans = "";
 
-function operate(num1, num2, operator) { 
-  let op1 = parseFloat(num1);
-  let op2 = parseFloat(num2);
-
-  if (operator === "+") { return add(op1,op2); }
-  if (operator === "-") { return sub(op1,op2); }
-  if (operator === "*") { return mlt(op1,op2); }
-  if (operator === "/") { return div(op1,op2); }
-  if (operator === "%") { return mod(op1,op2); }
-}
 
 
 /* operator functions */
@@ -21,28 +16,45 @@ const div = function(a,b) {
 }
 const mod = (a,b) => a % b;
 
-let str = "";
-let num1 = "";
-let num2 = "";
-let operator = "";
-let ans = "";
-console.log(num1);
-const btns = document.querySelectorAll('button');
-const display = document.querySelector('.display');
-console.log(display.textContent);
 
-btns.forEach((button) => {
-  button.addEventListener('click', (e) => {
-    console.log('clicked');
-    let value = button.textContent;
-    console.log(value);
-    str += value;
-    updateDisplay(str);
-    console.log(str);
+function operate(num1, num2, operator) { 
+  let op1 = parseFloat(num1);
+  let op2 = parseFloat(num2);
+  
+  if (operator === "+") { return add(op1,op2); }
+  if (operator === "-") { return sub(op1,op2); }
+  if (operator === "*") { return mlt(op1,op2); }
+  if (operator === "/") { return div(op1,op2); }
+  if (operator === "%") { return mod(op1,op2); }
+}
 
-  });
-});
+function clearDisplay() {
+  str = "";
+  updateDisplay(str);
+}
+
+function backSpace() {
+  str = str.substring(0, str.length-1); // remove last character.
+  updateDisplay(str);
+}
 
 function updateDisplay(val) {
   display.textContent = val;
+  console.log(display.textContent);
 }
+
+
+
+function buttonPress(e) {
+  console.log('clicked');
+  let value = e.target.textContent;
+  str += value;
+  updateDisplay(str);
+}
+
+const btns = document.querySelectorAll('button');
+const display = document.querySelector('.display');
+
+btns.forEach(button => button.addEventListener('click', buttonPress));
+
+
