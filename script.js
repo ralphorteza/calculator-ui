b3d3f26ece761017ff51c9fbd0ba75d5b28d4e6f
ralph orteza <ralphlorteza@gmail.com>
2022-07-19 07:05:50 -0700

    Fix large decimal number answers

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let str = "";
+let currentString = "";
 let num1 = "";
 let num2 = "";
 let operator = "";
 let ans = "";
+const ErrorStr = "Syntax Error";
 
 const regexNumbers = /[0-9]/g;
 const regexOperators = /[%*+/-]/g;
@@ -10,6 +11,7 @@ const regexEquals = /[=]/g;
 
 
 /* operator functions */
+const mod = (a,b) => a % b;
 const add = (a, b) => a + b;
 const sub = (a,b) => a - b;
 const mlt = (a,b) => a * b;
@@ -17,19 +19,22 @@ const div = function(a,b) {
   if (b !== 0) { return a / b;}
   return "lol";
 }
-const mod = (a,b) => a % b;
 
 /* Function to do operations of two given numbers. */
 function operate(num1, num2, operator) { 
-  let op1 = parseFloat(num1);
-  let op2 = parseFloat(num2);
+  let op1 = Number(num1);
+  let op2 = Number(num2);
   let result = 0;
   if (operator === "+") { result = add(op1,op2); }
   if (operator === "-") { result = sub(op1,op2); }
   if (operator === "*") { result = mlt(op1,op2); }
   if (operator === "/") { result = div(op1,op2); }
   if (operator === "%") { result = mod(op1,op2); }
-  return result;
+  return result.toFixed(2);
+}
+
+function checkOperation() {
+  return num1 === "" && num2 === "" && operator === "";
 }
 
 /* Function to clear display and assigned variables. */
@@ -40,7 +45,7 @@ function clearDisplay() {
 
 /* Resets variables to intial assignments. */
 function clearAssignVars() {
-  str = "";
+  currentString = "";
   num1 = "";
   num2 = "";
   operator = "";
@@ -50,8 +55,8 @@ function clearAssignVars() {
 /* Function to remove the last character of displayed String. */
 function backSpace(value) {
   let updateString = value.substring(0, value.length-1); // remove last character.
-  str = updateString;
-  updateDisplay(str);
+  currentString = updateString;
+  updateDisplay(currentString);
 }
 
 /* Function to update the display. */
@@ -64,33 +69,35 @@ function updateDisplay(val) {
 function getOperand2(val) {
   let unprocessed = val.match(/[%*+/-](.*)/g);
   let unprocessedStr = unprocessed[0];
-  return unprocessedStr.slice(1);
+  let temp = Number (unprocessedStr.slice(1));
+  console.log(typeof temp);
+  return temp;
 }
 
 /* Function to retrieve ans and others after pressing '=' */
 function getAns() {
-  num2 = getOperand2(str);
+  num2 = getOperand2(currentString);
   ans = operate(num1, num2, operator);
   console.log(num1 + " " + operator + " " + num2 + " = " + ans);
   num1 = ans;
-  str = ans;
-  console.log(str);
+  currentString = Number(ans);
+  console.log("current String: " + currentString);
   clearDisplay();
-  updateDisplay(str);
+  updateDisplay(currentString);
 }
 
 /* Function to assign the operator update String. */
 function assignOperator(val) {
   operator = val;
-  if (str !== "") { 
-    num1 = str; 
+  if (currentString !== "") { 
+    num1 = Number(currentString); 
   } else { 
     num1 = ans;
   }
   console.log("op1", num1);
   console.log("operator", operator);
-  str += val;
-  updateDisplay(str);
+  currentString += val;
+  updateDisplay(currentString);
 }
 
 function buttonPress(e) {
@@ -105,10 +112,10 @@ function buttonPress(e) {
     clearDisplay();
     clearAssignVars();
   } else if (value === "DEL") {
-    backSpace(str);
+    backSpace(currentString);
   } else {
-    str += value;
-    updateDisplay(str);
+    currentString += value;
+    updateDisplay(currentString);
   }
 
 }
